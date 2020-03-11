2884f1eccab3163056347ae2197be08dd887f9c7
AliaksandrTarashkevich <alexandertarashkevich@gmail.com>
2020-03-11 11:11:19 +0300

    hosting topic finished

---
+++ 04hosting.js
@@ -0,0 +1,24 @@
+// console.log(sum(1,41))
+
+// function sum(a,b) {
+//   return a + b
+// }
+
+// console.log(i)
+// var i = 42 // returns undefined
+
+// console.log(num)
+// const num = 3 // returns err
+
+//===============================
+//FUNCTION EXPRESSION FUNCTION DECLARATION
+// console.log(square(25))
+
+
+// function square(num) {
+//   return num ** 2
+// } // function declaration, returns 625
+
+// const square = num => {
+//   return num ** 25
+// } // function expression, returns err, before initialization
